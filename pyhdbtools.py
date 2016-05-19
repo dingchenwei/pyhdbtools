a6f658d89a5c9739242b04a08b367bfb5edd736b
Alex Bos <alexbos@Alexs-MacBook-Pro.local>
2016-05-18 21:14:50 -0500

    json config rewrite

--- pyhdbtools.py
+++ pyhdbtools.py
@@ -1,7 +1,82 @@
-import json, requests, sys, urllib2, sqlite3, getopt, os, textwrap, datetime
+import json, requests, sys, urllib2, sqlite3, getopt, os, textwrap, datetime, urllib
 from pprint import pprint
 from collections import OrderedDict
 from lxml import etree
+
+class JSONConfig:
+	def __init__(self):
+		self.cookiePresent = False
+		self.username = self.passkey = ''
+		self.outputdir = 'watchdir/'
+		self.cookie = OrderedDict([('uid',''),('pass',''),('hash','')])
+
+	def read(self, filename):
+		fileBasePath = os.path.dirname(os.path.realpath(__file__))
+		try:
+			with open(os.path.join(fileBasePath,filename),'r') as json_data:
+				jsonConfig = json.load(json_data)
+				json_data.close()
+		except IOError:
+			print "ERROR: config.json not found or not readable. Please run again with --makconf"
+			exit(1)
+		except:
+			print "ERROR: config.json is invalid. Please recreate with --makeconf"
+			exit(1)
+
+		self.username = jsonConfig['username']
+		self.passkey = jsonConfig['passkey']
+		self.outputdir = jsonConfig['outputdir']
+		if jsonConfig['cookie']['uid'] != "":
+			self.cookiePresent = True
+			self.c_uid = jsonConfig['cookie']['uid']
+			self.c_pass = jsonConfig['cookie']['pass']
+			self.c_hash = jsonConfig['cookie']['hash']
+			self.cookie = {'uid':self.c_uid,'pass':self.c_pass,'hash':self.c_hash}
+
+	def fileExists(self,filename):
+		fileBasePath = os.path.dirname(os.path.realpath(__file__))
+		try:
+			with open(os.path.join(fileBasePath,filename),'r') as json_data:
+				json_data.close()
+				return True
+		except IOError:
+			return False
+
+	def write(self, filename):
+		#cookieJson = OrderedDict([('uid',self.c_uid),('pass',self.c_pass),('hash',self.c_hash)])
+		fileBasePath = os.path.dirname(os.path.realpath(__file__))
+		data = OrderedDict([('username',self.username),('passkey',self.passkey),('outputdir',os.path.abspath(self.outputdir)),('cookie',self.cookie)])
+
+		try:
+			with open(os.path.join(fileBasePath,filename), 'w') as outfile:
+				json.dump(data, outfile, indent=4, separators=(',', ': '))
+		except IOError:
+			print "ERROR: Cannot write config.json"
+			exit(1)
+
+	def hasCookie(self):
+		return self.cookiePresent
+
+	#def validateUser(self):
+	#	i=0
+
+	def setCookie(self, cookie):
+		self.cookie = cookie
+		if cookie['uid'] != '' and cookie['pass'] != '' and cookie['hash'] != '':
+			self.cookiePresent = True
+		else:
+			self.cookiePresent = False
+
+	def getCookie(self):
+		return self.cookie
+
+	def setBaseConfig(self, config):
+		self.username = config['username']
+		self.passkey = config['passkey']
+		self.outputdir = config['outputdir']
+
+	def getBaseConfig(self):
+		return {'username':self.username,'passkey':self.passkey,'outputdir':self.outputdir}
 
 def isDownloaded(id):
 	#checks if the id is in the downloaded list
@@ -15,7 +90,7 @@ def isWatched(id):
 	cur.execute('SELECT * FROM watched WHERE id=?', (id,))
 	return False if len(cur.fetchall()) == 0 else True
 
-def fetchTorrent(id,watchdir,sslVerify=True,allowDupes=False):
+def fetchTorrent(id,outputdir,sslVerify=True,allowDupes=False):
 	#fetches torrent based on the given torrent id.  checks the database if it's already been downloaded.
 	#if not, it downloads it.
 	if isDownloaded(id) == False or allowDupes:
@@ -26,7 +101,7 @@ def fetchTorrent(id,watchdir,sslVerify=True,allowDupes=False):
 		torrentUrl = "https://hdbits.org/download.php?id=" + str(fetchData['data'][0]['id']) + "&passkey=" + passkey
 		idStr = str(fetchData['data'][0]['id'])
 		nameStr = fetchData['data'][0]['filename']
-		fullPath = os.path.join(watchdir,nameStr)
+		fullPath = os.path.join(outputdir,nameStr)
 		print "fetching: " + nameStr + " at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 		#save .torrent file
@@ -68,42 +143,61 @@ def populateWatchlist(queueFilename):
 	conn.commit()
 
 def generateConfigFile(sslVerify=True):
-	isCorrect = False
+	validResponse = cookieIsSet = False
 	fileBasePath = os.path.dirname(os.path.realpath(__file__))
-	usernameDefaultInput = ""
-	passkeyDefaultInput = "" 
-	watchdirDefaultInput = "watchdir/"
+
+	cfg = JSONConfig()
+	if cfg.fileExists('config.json'): 
+		cfg.read('config.json')
 
 	while True:
-		usernameInput = raw_input("Please input your hdbits username: [" + usernameDefaultInput + "] ")
-		passkeyInput = raw_input("Please input your hdbits passkey: [" + passkeyDefaultInput + "] ")
-		watchdirInput = raw_input(".torrent file output directory: [" + watchdirDefaultInput + "] ") 
+		usernameInput = raw_input("Please input your hdbits username: [" + cfg.getBaseConfig()['username'] + "] ")
+		passkeyInput = raw_input("Please input your hdbits passkey: [" + cfg.getBaseConfig()['passkey'] + "] ")
+		outputdirInput = raw_input(".torrent file output directory: [" + cfg.getBaseConfig()['outputdir'] + "] ")
 
 		#if no input, go with default input
-		if len(usernameInput) == 0:
-			usernameInput = usernameDefaultInput
-		if len(passkeyInput) == 0:
-			passkeyInput = passkeyDefaultInput
-		if len(watchdirInput) == 0:
-			watchdirInput = watchdirDefaultInput
-		absWatchdir = os.path.abspath(watchdirInput)
+		if len(usernameInput) == 0: usernameInput = cfg.getBaseConfig()['username']
+		if len(passkeyInput) == 0: passkeyInput = cfg.getBaseConfig()['passkey']
+		if len(outputdirInput) == 0: outputdirInput = cfg.getBaseConfig()['outputdir']
+		absOutputdir = os.path.abspath(outputdirInput)
 
-		#Checking watchdir path
-		if os.path.exists(absWatchdir) == False:
+		#Checking outputdir path
+		if os.path.exists(absOutputdir) == False:
 			while True:
-				a =  raw_input("Warning: path does not exist, create? (y/n) ")
+				a = raw_input("Warning: path does not exist, create? (y/n) ")
 				if a == 'y':
 					try:
-						os.makedirs(absWatchdir)
+						os.makedirs(absOutputdir)
 					except:
 						print "ERROR: Could not create directory"
 					break
 				elif a == 'n':
 					break
+		
+		#configure cookie
+		while True:
+			a = raw_input("Would you like to set a cookie? (y/[n]) ")
+			if a == 'y':
+				cookieIsSet = True
+				c_uid = raw_input("Please input your cookie uid: [" + cfg.getCookie()['uid'] + "] ")
+				c_pass = raw_input("Please input your cookie pass: [" + cfg.getCookie()['pass'] + "] ")
+				c_hash = raw_input("Please input your cookie hash: [" + cfg.getCookie()['hash'] + "] ")
+				#if no response leave it unchanged
+				if len(c_uid) == 0: c_uid = cfg.getCookie()['uid']
+				if len(c_pass) == 0: c_pass = cfg.getCookie()['pass']
+				if len(c_hash) == 0: c_hash = cfg.getCookie()['hash']
+				break
+			else:
+				c_uid = c_pass = c_hash = ''
+				break
 
 		print "\nUsername: " + usernameInput
 		print "Passkey: " + passkeyInput
-		print "Output Directory: " + os.path.abspath(watchdirInput)
+		print "Output Directory: " + os.path.abspath(outputdirInput)
+		if cookieIsSet:
+			print "cookie uid = " + c_uid
+			print "cookie pass = " + c_pass
+			print "cookie hash = " + c_hash
 
 		#check if data input is correct
 		while True:
@@ -116,31 +210,23 @@ def generateConfigFile(sslVerify=True):
 				response = requests.post(apiUrl, data=json.dumps(payload), headers=headers, verify=sslVerify)
 				testData = json.loads(response.text)
 				if testData['status'] == 0:
-					isCorrect = True
+					validResponse = True
 					break
 				else:
 					print "ERROR: API authentication failure. Check username and passkey and try again"
 					break
 			elif a == 'n':
 				#save inputs as defaults for next try
-				usernameDefaultInput = usernameInput
-				passkeyDefaultInput = passkeyInput
-				watchdirDefaultInput = watchdirInput
+				#cfg.setCookie({'uid':c_uid,'pass':c_pass,'hash':c_hash})
+				#cfg.setBaseConfig({'username':usernameInput,'passkey':passkeyInput,'outputdir':outputdirInput})
 				break
-		if isCorrect:
+		if validResponse:
 			break
 
 	#write config file
-	data = OrderedDict([('username',usernameInput),('passkey',passkeyInput),('output_dir',os.path.abspath(watchdirInput))])
-	try:
-		with open(os.path.join(fileBasePath,'config.json'), 'w') as outfile:
-			json.dump(data, outfile, indent=4, separators=(',', ': '))
-	except IOError:
-		print "ERROR: Cannot write config.json"
-		exit(1)
-
-	#remove world read from config file to keep passkey a little more secure
-	os.chmod(os.path.join(fileBasePath,'config.json'), 0660)
+	cfg.setCookie({'uid':c_uid,'pass':c_pass,'hash':c_hash})
+	cfg.setBaseConfig({'username':usernameInput,'passkey':passkeyInput,'outputdir':outputdirInput})
+	cfg.write('config.json')
 	exit(0)
 
 def displayHelp():
@@ -185,18 +271,28 @@ def displayHelp():
     """)
 	exit(0)
 
+def scrapeFeaturedQueue():
+	cfg = JSONConfig()
+	cfg.read('json.config')
+	url = 'https://hdbits.org/featuredqueue.php'
+	cookie = cfg.getCookie()
+	r = requests.post(url, cookies=cookie)
+	fout = open('q.html', "wb")
+	fout.write(r.text.encode('utf8'))
+	fout.close()
+
 def main():
-	global username, passkey, headers, verbose, conn, debug
+	global headers, verbose, conn, debug
 	
-	VERSION = 'build 051716 alpha'
+	VERSION = 'build 051816 alpha'
 
 	#default options
 	makeConf = updateFeatured = fetchFeatured = verbose = showVersion = dispHelp = allowDupes = singleTorrent = False
-	fetchFree = debug = False
+	fetchFree = debug = getQueue = False
 	sslVerify = True
 
 	#argument option handling
-	options, remainder = getopt.getopt(sys.argv[1:], 'u:hs:VfFv', ['update-featured=','fetch-featured','makeconf',
+	options, remainder = getopt.getopt(sys.argv[1:], 'u:hs:VfFvq', ['update-featured=','fetch-featured','makeconf',
 		'noverify','help','single-torrent','allowdupes','fetch-free','version','debug'])
 	for opt, arg in options:
 		if opt in ('-v'):
@@ -224,6 +320,8 @@ def main():
 		elif opt in ('--debug'):
 			debug = True
 			verbose = True
+		elif opt in ('-q'):
+			getQueue = True
 
 	if debug:
 		print "starting run at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -240,20 +338,12 @@ def main():
 
 	#importing json.config
 	fileBasePath = os.path.dirname(os.path.realpath(__file__))
-	try:
-		with open(os.path.join(fileBasePath,'config.json'),'r') as json_data:
-			jsonConfig = json.load(json_data)
-			json_data.close()
-	except IOError:
-		print "ERROR: config.json not found or not readable. Please run again with --makconf"
-		exit(1)
-	except:
-		print "ERROR: config.json is invalid. Please recreate with --makeconf"
-		exit(1)
+	cfg = JSONConfig()
+	cfg.read('config.json')
 
-	username = jsonConfig['username']
-	passkey = jsonConfig['passkey']
-	watchdir = jsonConfig['output_dir']
+	username = cfg.getBaseConfig()['username']
+	passkey = cfg.getBaseConfig()['passkey']
+	outputdir = cfg.getBaseConfig()['outputdir']
 
 	headers = {'content-type': 'application/json'}
 
@@ -268,11 +358,19 @@ def main():
 	conn.execute("CREATE TABLE IF NOT EXISTS watched(id INT, name TEXT)")
 
 	if singleTorrent:
-		fetchTorrent(torrentID, watchdir,sslVerify=sslVerify,allowDupes=allowDupes)
+		fetchTorrent(torrentID, outputdir ,sslVerify=sslVerify,allowDupes=allowDupes)
 		exit(1)
 
 	if updateFeatured:
 		populateWatchlist(queueFilename)
+
+	if getQueue:
+		#scrapeFeaturedQueue()
+		if cfg.hasCookie():
+			print "has cookie"
+		else:
+			print "no cookie"
+		exit(1)
 
 	#fetch any freeleech in newest 30
 	if fetchFree:
@@ -283,7 +381,7 @@ def main():
 
 		for x in torrentData['data']:
 			if x['freeleech'] == 'yes':
-				fetchTorrent(x['id'],watchdir,sslVerify=sslVerify,allowDupes=allowDupes)
+				fetchTorrent(x['id'],outputdir,sslVerify=sslVerify,allowDupes=allowDupes)
 
 	#Checks the first 7 entries in the watchlist and downloads them if freeleech
 	if fetchFeatured:
@@ -299,7 +397,7 @@ def main():
 				conn.execute('DELETE FROM watched WHERE id=?', (row[1],))
 				conn.commit()
 			elif torrentData['data'][0]['freeleech'] == "yes":
-				fetchTorrent(torrentData['data'][0]['id'],watchdir,sslVerify=sslVerify,allowDupes=allowDupes)
+				fetchTorrent(torrentData['data'][0]['id'],outputdir,sslVerify=sslVerify,allowDupes=allowDupes)
 				conn.execute('DELETE FROM watched WHERE id=?', (row[1],))
 				conn.commit()
 			i+=1
@@ -307,7 +405,7 @@ def main():
 			print "Warning: watchlist is empty"
 		cur.close()
 
-	if not (updateFeatured or fetchFeatured or fetchFree or singleTorrent or makeConf or showVersion or dispHelp):
+	if not (updateFeatured or fetchFeatured or fetchFree or singleTorrent or makeConf or showVersion or dispHelp or getQueue):
 		print "ERROR: No runmode specified"
 		displayHelp()
 
